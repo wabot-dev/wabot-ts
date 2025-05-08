25c36dba6a140d8df6d6bb18c6d5bcf059563e2f
Jorge Narvaez <jormelcn@gmail.com>
2025-05-08 00:13:14 -0500

    fix: standardize error message formatting in WhatsAppProdConnection methods

--- src/channels/whatsapp/WhatsAppProdConnection.ts
+++ src/channels/whatsapp/WhatsAppProdConnection.ts
@@ -1,17 +1,16 @@
-import { singleton } from "tsyringe";
-import type { IWhatsAppConnection, IWhatsAppMessageListener } from "./IWhatsAppConnection";
-import type { IChatMessage } from "@/core";
+import { singleton } from 'tsyringe'
+import type { IWhatsAppConnection, IWhatsAppMessageListener } from './IWhatsAppConnection'
+import type { IChatMessage } from '@/core'
 
 @singleton()
 export class WhatsAppProdConnection implements IWhatsAppConnection {
   listenMessage(businessNumber: string, listener: IWhatsAppMessageListener): void {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
   sendWhatsApp(businessNumber: string, to: string, replyMessage: IChatMessage): Promise<void> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
   connect(): void {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
-
 }
