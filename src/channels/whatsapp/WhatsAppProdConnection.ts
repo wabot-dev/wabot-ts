05de9e50f85d3c40029e55d0a8bbde26eb2d5240
Jorge Narvaez <jormelcn@gmail.com>
2025-05-08 23:17:01 -0500

    chore: update version to 0.0.14 in package.json and package-lock.json refactor: clean up constructor in WhatsAppProdConnection class

--- src/channels/whatsapp/WhatsAppProdConnection.ts
+++ src/channels/whatsapp/WhatsAppProdConnection.ts
@@ -6,17 +6,11 @@ import type { IWhatsAppConnection, IWhatsAppMessageListener } from './IWhatsAppC
 
 @singleton()
 export class WhatsAppProdConnection implements IWhatsAppConnection {
-  
   constructor(@inject(ExpressApp) private express: Express) {
-    
     this.express.get('/try-express', (req, res) => {
-      
       res.status(200).json({ success: true })
-   
     })
-
   }
-
 
   listenMessage(businessNumber: string, listener: IWhatsAppMessageListener): void {
     throw new Error('Method not implemented.')
