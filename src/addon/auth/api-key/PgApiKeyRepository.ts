99050b069fbb698d9d514b777b17f8eef36aa4d3
Jorge Narvaez <jormelcn@gmail.com>
2025-09-13 00:20:10 -0500

    Fix PgApiKeyRepository

--- src/addon/auth/api-key/PgApiKeyRepository.ts
+++ src/addon/auth/api-key/PgApiKeyRepository.ts
@@ -2,12 +2,14 @@ import { IApiKeyRepository } from './IApiKeyRepository'
 import { ApiKey } from './ApiKey'
 import { Pool } from 'pg'
 import { PgCrudRepository } from '@/feature/pg'
+import { singleton } from 'tsyringe'
 
+@singleton()
 export class PgApiKeyRepository extends PgCrudRepository<ApiKey> implements IApiKeyRepository {
   constructor(pool: Pool) {
     super(pool, {
       schema: 'wabot',
-      table: 'apy_key',
+      table: 'api_key',
       constructor: ApiKey,
     })
   }
