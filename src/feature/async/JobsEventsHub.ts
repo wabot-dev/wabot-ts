608478d0b64cafb681bbb6a1b797762def976359
Jorge Narvaez <jormelcn@gmail.com>
2025-09-14 22:18:30 -0500

    Fix

--- src/feature/async/JobsEventsHub.ts
+++ src/feature/async/JobsEventsHub.ts
@@ -1,6 +1,7 @@
 import { IStorableData } from '@/core/storable'
 import { Job } from './Job'
 import { singleton } from '@/core/injection'
+import { Logger } from '@/core/logger'
 
 export interface IJobEvent extends IStorableData {
   jobId: string
@@ -11,18 +12,26 @@ export type IJobEventListener = (event: IJobEvent) => void | Promise<void>
 
 @singleton()
 export class JobsEventsHub {
+  private logger = new Logger('wabot:jobs-events-hub')
   private jobsEventsListener: IJobEventListener | null = null
 
   notifyJobCreated(job: Job) {
-    if (!this.jobsEventsListener) {
-      return
-    }
-
-    this.jobsEventsListener({
-      jobId: job.id,
-      commandName: job.commandName,
-      type: 'created',
-    })
+    const timer = setTimeout(async () => {
+      if (!this.jobsEventsListener) {
+        return
+      }
+      try {
+        await this.jobsEventsListener({
+          jobId: job.id,
+          commandName: job.commandName,
+          type: 'created',
+        })
+      } catch (err) {
+        this.logger.error(err)
+      } finally {
+        clearTimeout(timer)
+      }
+    }, 1000)
   }
 
   listenJobsEvents(listener: IJobEventListener) {
